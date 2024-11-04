1b9c97a326327eba9143b40bde42d7951d13b2a5
Key <tavik002@gmail.com>
2024-11-04 22:52:44 +0900

    fix symlink for detect correction

--- scripts/install.js
+++ scripts/install.js
@@ -19,37 +19,37 @@ const { getDotaPath } = require('./utils');
     }
 
     for (const directoryName of ['game', 'content']) {
-        const sourcePath = path.resolve(__dirname, '..', directoryName);
-        assert(fs.existsSync(sourcePath), `Could not find '${sourcePath}'`);
+        const sourcePath = path.join(dotaPath, directoryName, 'dota_addons', addon_name);
+        const targetPath = path.resolve(__dirname, '..', directoryName);
+        assert(fs.existsSync(targetPath), `Could not find '${targetPath}'`);
 
         const targetRoot = path.join(dotaPath, directoryName, 'dota_addons');
         assert(fs.existsSync(targetRoot), `Could not find '${targetRoot}'`);
 
-        const targetPath = path.join(dotaPath, directoryName, 'dota_addons', addon_name);
+        console.log('sourcePath:', sourcePath);
+        console.log('targetPath:', targetPath);
+        // targetPath: D:\Development\Dota2CustomMap\dota-hero-race-2\game
+        // sourcePath: C:\Game\Steam\steamapps\common\dota 2 beta\game\dota_addons\dota_hero_race_2
 
-        // console.log('sourcePath:', sourcePath);
-        // console.log('targetPath:', targetPath);
-        if (fs.existsSync(targetPath)) {
+        if (fs.existsSync(sourcePath)) {
             const isCorrect = fs.lstatSync(sourcePath).isSymbolicLink() && fs.realpathSync(sourcePath) === targetPath;
             if (isCorrect) {
                 console.log(`Skipping '${sourcePath}' since it is already linked`);
                 continue;
             } else {
-                // 移除目标文件夹的所有内容，
-                console.log(`'${targetPath}' is already linked to another directory, removing`);
-                fs.chmodSync(targetPath, '0755');
-                rimraf(targetPath, () => {
-                    console.log('removed target path');
-                    fs.moveSync(sourcePath, targetPath);
+                // Remove all contents of source directory
+                console.log(`'${sourcePath}' is already linked to another directory, removing`);
+                fs.chmodSync(sourcePath, '0755');
+                rimraf(sourcePath, () => {
+                    console.log('removed source path: ' + sourcePath);
                     fs.symlinkSync(targetPath, sourcePath, 'junction');
-                    console.log(`Repaired broken link ${sourcePath} <==> ${targetPath}`);
+                    console.log(`Repaired broken link ${sourcePath} ==> ${targetPath}`);
                 });
             }
         } else {
             // fs.moveSync(sourcePath, targetPath);
-            // fs.symlinkSync(targetPath, sourcePath, 'junction');
-            fs.symlinkSync(sourcePath, targetPath, 'junction');
-            console.log(`Linked ${sourcePath} <==> ${targetPath}`);
+            fs.symlinkSync(targetPath, sourcePath, 'junction');
+            console.log(`Linked ${sourcePath} ==> ${targetPath}`);
         }
     }
 
